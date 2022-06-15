9ca5b788a15651dc799b9a066cc46491b788b3e7
owentr1369 <tamvaa1306@gmail.com>
2022-06-15 11:42:23 +0700

    active tabs and panes

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const $$ = document.querySelectorAll.bind(document);
 const tabs = $$(".tab-item");
 const panes = $$(".tab-pane");
 
-tabs.forEach((tab, index)=> {
-    tab.onclick = function(){
-        console.log(this)
-    }
-})
+tabs.forEach((tab, index) => {
+    const pane = panes[index]
+  tab.onclick = function () {
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
+    this.classList.add("active");
+    pane.classList.add("active")
+  };
+});
